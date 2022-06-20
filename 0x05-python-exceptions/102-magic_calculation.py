f760350344907a15e4dcd44ef7ef2b698c952cfe
aliciahvivian <vivianalice11@gmail.com>
2022-06-20 19:16:20 +0300

    Python bytecode

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
-
-
 def magic_calculation(a, b):
-    """Perform magic calculation"""
     result = 0
     for i in range(1, 3):
         try:
@@ -13,4 +10,4 @@ def magic_calculation(a, b):
         except:
             result = b + a
             break
-    return result
+    return 
