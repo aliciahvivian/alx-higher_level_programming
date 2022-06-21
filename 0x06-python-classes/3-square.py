768cd084a5ca4549da19d619796fbff6eedfc5eb
aliciahvivian <vivianalice11@gmail.com>
2022-06-22 01:30:44 +0300

    added Public instance method that returns the current square area

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""create an empty class called Square"""
+
+
+class Square:
+    """create a square.
+    size (no type/value verification)
+    atributes:
+    size: private instance.
+    """
+
+    def __init__(self, size=0):
+        self.__size = size
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        return self.__size * self.__size
